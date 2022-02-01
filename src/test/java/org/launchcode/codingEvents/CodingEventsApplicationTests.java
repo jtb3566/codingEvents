e727080dda8558958038261234dce9f253b1d7b3
James Ballard <ballardj77@gmail.com>
2022-02-01 13:46:48 -0600

    Initial commit

---
+++ src/test/java/org/launchcode/codingEvents/CodingEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
